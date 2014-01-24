ab0873d730b5b081f0c34fe62771f73634f82c8a
vagrant <kaaretas@gmail.com>
2014-01-24 14:16:22 +0000

    Make a basic User model (including secure passwords)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,15 +4,22 @@ describe "Static pages" do
 
   subject { page }
 
-  describe "Home page" do
-    before { visit root_path }
-
-    it { should have_content('Sample App') }
-    it { should have_title(full_title('')) }
-    it { should_not have_title('|Home') }
+  shared_examples_for "all static pages" do
+    it { should have_content(heading) }
+    it { should have_title(full_title(page_title)) }
   end
 
-describe "Help page" do
+  describe "Home page" do
+    before { visit root_path }
+    let(:heading)    { 'Sample App' }
+    let(:page_title) { '' }
+
+    it_should_behave_like "all static pages"
+    it { should_not have_title('| Home') }
+  end
+
+
+  describe "Help page" do
     before { visit help_path }
 
     it { should have_content('Help') }
@@ -32,5 +39,21 @@ describe "Help page" do
     it { should have_content('Contact') }
     it { should have_title(full_title('Contact')) }
   end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to # ここにコードを記入
+    click_link "Contact"
+    expect(page).to # ここにコードを記入
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to # ここにコードを記入
+    click_link "sample app"
+    expect(page).to # ここにコードを記入
+  end
+
 
 end
