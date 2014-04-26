47f41aa89ceeb0172c2c522fe830f628c34e0305
vagrant <kaaretas@gmail.com>
2014-04-26 13:02:13 +0000

    Add user microposts

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,6 @@
 module SessionsHelper
     
-    def  sign_in(user)
+    def sign_in(user)
         remember_token = User.new_remember_token
         cookies.permanent[:remember_token] = remember_token
         user.update_attribute(:remember_token, User.encrypt(remember_token))
@@ -27,6 +27,13 @@ module SessionsHelper
     
     def current_user?(user)
         user == current_user
+    end
+    
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice:"Please sign in."
+      end
     end
     
     def redirect_back_or(default)
--- spec/factory.rb
+++ spec/factory.rb
@@ -4,5 +4,15 @@ FactoryGirl.define do
     email "michael@example.com"
     password "foobar"
     password_confirmation "foobar"
+
+    factory :admin do
+      admin true
+    end
   end
+
+  factory :micropost do
+    content "Lorem ipsum"
+    user
+  end
+
 end
